bb1166318684671cf962ecbb0c768e0aca74610e
Hassat-Hunter <44263352+Hassat-Hunter@users.noreply.github.com>
2018-10-18 15:03:28 +0200

    Add files via upload

--- Update_Those_Mods.lua
+++ Update_Those_Mods.lua
@@ -4,7 +4,7 @@ if UpdateThisMod then
 		data = {
 			modworkshop_id = 21098,
 			dl_url = "https://github.com/PJzuza/Hassat_TweakdataFixed/raw/master/updates/TweakdataFixed.zip", 
-			info_url = "https://raw.githubusercontent.com/Hassat_TweakdataFixed/mod.txt"
+			info_url = "https://github.com/PJzuza/Hassat_TweakdataFixed/raw/master/mod.txt"
 		}
 	})
 end
