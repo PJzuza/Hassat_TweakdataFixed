5e19f5222fb28dc65ddd4f658f475054a5185bb5
Hassat-Hunter <44263352+Hassat-Hunter@users.noreply.github.com>
2018-10-18 15:26:29 +0200

    Add files via upload

--- Update_Those_Mods.lua
+++ Update_Those_Mods.lua
@@ -1,6 +1,6 @@
 if UpdateThisMod then
 	UpdateThisMod:Add({
-		mod_id = 'Tweakdata fixed',
+		mod_id = 'Tweakdata Fixed',
 		data = {
 			modworkshop_id = 21098,
 			dl_url = "https://github.com/PJzuza/Hassat_TweakdataFixed/raw/master/updates/TweakdataFixed.zip", 
